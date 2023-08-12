b73f861e2bb9fdd2af1201605f7d549b5b40a809
SafeHavenRoleplay <therealglitchxy@gmail.com>
2023-08-11 22:20:42 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,5 +1,5 @@
 -- Config.lua
--- 🌟 Created with passion and ingenuity by ✨ SuperintendentSinclair ✨
+-- Created by JemmaSinclair
 
 Config = {}
 
@@ -8,7 +8,7 @@ Config.Keybind = "numpad2"
 -- Define the stages and park pattern for each vehicle
 Config.Vehicles = {
 	-- State
-    ["sasp1"] = {
+    ["gamename"] = {
 		Repair = true,
         Stages = {
             [1] = {
@@ -16,19 +16,48 @@ Config.Vehicles = {
                 ,[4] = false
                 ,[3] = true
                 ,[5] = true
-            }, -- Stage 1: All extras off
+            },
             [2] = {
                  [2] = true
                 ,[4] = false
                 ,[3] = true
                 ,[5] = true
-            },  -- Stage 2: Extra 1 on, others off
+            }, 
             [3] = {
                  [2] = true
                 ,[4] = true
                 ,[3] = true
                 ,[5] = true
-            },   -- Stage 3: Extra 1 and 2 on, extra 3 off
+            }, 
+            -- Add more stages as needed for this vehicle
+        },
+        ParkPattern = { -- NOT YET IMPLEMENTED
+            [1] = true,
+            [2] = false,
+            [3] = true
+        },  -- Park Pattern: Extra 1 and 3 on, extra 2 off
+    },
+	["gamename"] = { -- Second Car
+		Repair = true,
+        Stages = {
+            [1] = {
+                 [2] = false
+                ,[4] = false
+                ,[3] = true
+                ,[5] = true
+            },
+            [2] = {
+                 [2] = true
+                ,[4] = false
+                ,[3] = true
+                ,[5] = true
+            }, 
+            [3] = {
+                 [2] = true
+                ,[4] = true
+                ,[3] = true
+                ,[5] = true
+            }, 
             -- Add more stages as needed for this vehicle
         },
         ParkPattern = { -- NOT YET IMPLEMENTED
